616b42d1270b568bead0a836a6035b91332c7454
swys <abacus822@gmail.com>
2013-08-05 13:53:10 -0400

    Initial commit

---
+++ watercolor.js
@@ -0,0 +1,64 @@
+var Transform = require('stream').Transform,
+    os = require('os'),
+    colors = {
+        black : '\u001b[30m',
+        gray : '\u001b[90m',
+        red : '\u001b[91m',
+        green : '\u001b[92m',
+        yellow : '\u001b[93m',
+        blue : '\u001b[94m',
+        magenta : '\u001b[95m',
+        cyan : '\u001b[96m',
+        white : '\u001b[37m',
+        reset : '\u001b[0m',
+        error : '\u001b[91m',
+        warn : '\u001b[93m',
+        success : '\u001b[92m'
+    },
+    styles = {
+        underline : '\u001b[4m',
+        blink : '\u001b[5m'
+    };
+
+function addNewLine(string) {
+    if (/[\n\r]/.test(string)) {
+        return string;
+    } else {
+        return string + '\n';
+    }
+}
+
+function Watercolor(opts) {
+    if (!(this instanceof Watercolor)) {
+        return new Watercolor();
+    }
+    this.color = colors[opts.color] || '';
+    this.style = styles[opts.style] || '';
+    Transform.call(this, { decodeStrings : false });
+}
+
+Watercolor.prototype = Object.create(Transform.prototype, {
+    constructor : {
+        value : Watercolor
+    }
+});
+
+Watercolor.prototype._transform = function(chunk, encoding, next) { 
+    this.push(this.style + this.color + addNewLine(chunk) + colors.reset);
+    next();
+};
+
+Watercolor.prototype.setOpts = function(opts) {
+    if (typeof opts !== 'object') {
+        this.emit('error', "must pass options object into setOpts");
+    }
+    this.color = colors[opts.color] || this.color;
+    this.style = styles[opts.style] || this.style;
+};
+
+module.exports = function(opts) {
+        return new Watercolor(opts);
+};
+
+
+
