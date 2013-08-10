030bc32e76baf6a559276d0446c4b38c1753e44a
swys <abacus822@gmail.com>
2013-08-10 12:51:09 -0400

    Made API chainable so you can do watercolor.color('red').style('underline')...added test for this too

--- watercolor.js
+++ watercolor.js
@@ -1,15 +1,16 @@
 var Transform = require('stream').Transform,
     codes = require('./colors.js');
+
 function Watercolor(opts) {
     if (!(this instanceof Watercolor)) {
-        return new Watercolor();
+        return new Watercolor(opts);
     }
     if (opts) {
-        this.color = codes.colors[opts.color] || '';
-        this.style = codes.styles[opts.style] || '';
+        this._color = codes.colors[opts.color] || '';
+        this._style = codes.styles[opts.style] || '';
     } else {
-        this.color = '';
-        this.style = '';
+        this._color = '';
+        this._style = '';
     }
     Transform.call(this, { encoding : 'utf-8' });
 }
@@ -21,49 +22,45 @@ Watercolor.prototype = Object.create(Transform.prototype, {
 });
 
 Watercolor.prototype._transform = function(chunk, encoding, next) { 
-    this.push(this.color + this.style + chunk + codes.reset);
+    this.push(this._color + this._style + chunk + codes.reset);
     next();
 };
 
-Watercolor.prototype.setOpts = function(opts) {
-    if (typeof opts !== 'object' && typeof opts !== 'string') {
-        this.emit('error', "must pass options object or string into setOpts method");
-        return;
+Watercolor.prototype.color = function(setcolor) {
+    if (isValid.call(this, codes.colors, 'color', setcolor)) {
+        this._color = codes.colors[setcolor];
+        return this;
+    } else {
+        return this;
     }
-    if (typeof opts !== 'string' && typeof opts !== 'object') {
-        this.emit('error', "must pass options object or string into setOpts method");
-        return;
+};
+
+Watercolor.prototype.style = function(setstyle) {
+    if (isValid.call(this, codes.styles, 'style', setstyle)) {
+        this._style = codes.styles[setstyle];
+        return this;
+    } else {
+        return this;
     }
-    if (typeof opts === 'string' && opts === 'normal') {
-        this.color = '';
-        this.style = '';;
-        return;
-    }
-    if (opts.color === 'normal') {
-        this.color = '';
-        if (opts.style === 'normal') {
-            this.style = '';
-            return;
-        }
-        this.style = codes.styles[opts.style] || this.style;
-        return;
-    }
-    if (opts.style === 'normal') {
-        this.style = '';
-        if (opts.color === 'normal') {
-            this.style = '';
-            return;
-        }
-        this.color = codes.colors[opts.color] || this.color;
-        return;
-    }
-    this.color = codes.colors[opts.color] || this.color;
-    this.style = codes.styles[opts.style] || this.style;    
 };
 
 module.exports = function(opts) {
         return new Watercolor(opts);
 };
 
+function isValid(codeType, setter, arg) {
+    if (arg === 'normal') {
+        return true;
+    }
+    if (typeof arg !== 'string') {
+        this.emit('error', "Invalid arguments! You must pass a string to this function");
+        return false;
+    }       
+    if (!codeType[arg]) {
+        this.emit('error', arg + " not found! Pass in a valid " + setter);
+        return false;
+    }
+    return true;
+}
 
 
